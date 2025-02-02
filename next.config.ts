51201495fa9087eaffec9d09d530b1261e52e133
HusbanTech12 <husbantech08@gmail.com>
2025-02-03 02:57:46 +0500

    add next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
-    images: {
-      domains: ["cdn.sanity.io"],
-    },
-
-  
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
 };
 
-module.exports = nextConfig;
+module.exports = nextConfig;  // ✅ Static Export Mode hata diya
